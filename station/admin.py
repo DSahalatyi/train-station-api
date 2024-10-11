0c6e180630cfd356abb1f3ded8a3f56f84b109d5
DSahalatyi <dsahalatyi@gmail.com>
2024-10-11 14:44:57 +0300

    Add Route model, update Station model

--- station/admin.py
+++ station/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from station.models import Station
+from station.models import Station, Route
 
 
 @admin.register(Station)
 class StationAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Route)
+class RouteAdmin(ModelAdmin):
     pass
--- station/models.py
+++ station/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -5,3 +6,31 @@ class Station(models.Model):
     name = models.CharField(max_length=63)
     latitude = models.FloatField()
     longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station, on_delete=models.RESTRICT, related_name="source_routes"
+    )
+    destination = models.ForeignKey(
+        Station, on_delete=models.RESTRICT, related_name="destination_routes"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source.name} - {self.destination.name} ({str(self.distance)} km)"
+
+    def clean(self):
+        if self.source == self.destination:
+            raise ValidationError("Source and destination should be different")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=("source", "destination"),
+                name="unique_route",
+            )
+        ]
