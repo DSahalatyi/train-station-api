bdd2f09cc4205bda9be179f05a4a0fb5a54bfeb7
DSahalatyi <dsahalatyi@gmail.com>
2024-10-13 17:49:53 +0300

    Add throttling on token acquisition view

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -153,6 +153,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 50,
+    "DEFAULT_THROTTLE_CLASSES": ("user.throttling.LoginFailRateThrottle",),
 }
 
 # SimpleJWT settings
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
+from user.jwt_views import CustomTokenObtainPairView
 from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
@@ -13,7 +13,7 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("me/", ManageUserView.as_view(), name="manage"),
     # SimpleJWT
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
